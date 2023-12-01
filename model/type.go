1d39b4a413922fc838bfbde5d37a08cf5a98da1a
Rolly Maulana Awangga <ardiva.diva30@gmail.com>
2023-12-02 01:17:49 +0700

    mo

---
+++ model/type.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Username   string             `bson:"username" json:"username"`
+	Password   string             `bson:"password" json:"password"`
+}
+
+type Registrasi struct {
+	Nama_lengkap    string `bson:"nama_lengkap,omitempty" json:"nama_lengkap,omitempty"`
+	No_telp 		string `bson:"no_telp,omitempty" json:"no_telp,omitempty"`
+	TTL 			string `bson:"ttl,omitempty" json:"ttl,omitempty"`
+	NIM 			string `bson:"nim,omitempty" json:"nim,omitempty"`
+	Alamat 			string `bson:"alamat,omitempty" json:"alamat,omitempty"`
+}
+
+type Reservasi struct {
+	ID            primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Nama 		  string             `bson:"nama,omitempty" json:"nama,omitempty"`
+	No_telp		  string             `bson:"no_telp,omitempty" json:"no_telp,omitempty"`
+	TTL     	  string             `bson:"ttl,omitempty" json:"ttl,omitempty"`
+	Status	      string             `bson:"status,omitempty" json:"status,omitempty"`
+	Keluhan	      string             `bson:"keluhan,omitempty" json:"keluhan,omitempty"`
+}
+
+type Response struct {
+	Status  bool   `json:"status" bson:"status"`
+	Message string `json:"message,omitempty" bson:"message,omitempty"`
+}
